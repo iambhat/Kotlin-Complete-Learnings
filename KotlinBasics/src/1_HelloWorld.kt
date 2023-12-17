143ad185750089a46945af8be35c43732ce49a7c
Shreehari <explorethesource@gmail.com>
2023-12-17 21:13:07 +0530

    Hello World Program

--- KotlinBasics/src/1_HelloWorld.kt
+++ KotlinBasics/src/1_HelloWorld.kt
@@ -1,5 +1,15 @@
 // Hello World Program
 
+// An entry point of a Kotlin application is the main function.
+// There are two ways of writing the main function in Kotlin.
+
+// Method : 1
+
+//fun main(args: Array<String>) {
+//    println(args.contentToString())
+//}
+
+// Method : 2
 
 fun main() {
     println("Hello World!")
